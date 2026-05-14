85ca1967398d0fcc437c390d137c690ddb5dc37d
G Harshitha Reddy <harshithareddy967@gmail.com>
2026-05-14 19:35:06 +0530

    Implement KMP algorithm for substring search

---
+++ day53.py
@@ -0,0 +1,32 @@
+class Solution:
+    def search(self, a, b):
+        n = len(a)
+        m = len(b)
+        ans = []
+
+        # KMP preprocessing (LPS array)
+        lps = [0] * m
+        j = 0
+
+        for i in range(1, m):
+            while j > 0 and b[i] != b[j]:
+                j = lps[j - 1]
+
+            if b[i] == b[j]:
+                j += 1
+                lps[i] = j
+
+        # KMP search
+        j = 0
+        for i in range(n):
+            while j > 0 and a[i] != b[j]:
+                j = lps[j - 1]
+
+            if a[i] == b[j]:
+                j += 1
+
+            if j == m:
+                ans.append(i - m + 1)
+                j = lps[j - 1]
+
+        return ans
